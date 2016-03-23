d404240351dda1f2f84238e406b252cf501167c7
estrepan <strepan@mail.ru>
2016-03-23 18:13:50 +0200

    modal fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -157,8 +157,14 @@ module.exports = function (grunt) {
                 src: [
                     'bower_components/jquery/dist/jquery.min.js',
                     'bower_components/bootstrap/dist/js/bootstrap.min.js',
-                    'bower_components/angular/*.min.js',
-                    'bower_components/angular-*/*.min.js',
+                    'bower_components/angular/angular.min.js',
+                    'bower_components/angular-animate/angular-animate.min.js',
+                    'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+                    'bower_components/angular-cookies/angular-cookies.min.js',
+                    'bower_components/angular-resource/angular-resource.min.js',
+                    'bower_components/angular-route/angular-route.min.js',
+                    'bower_components/angular-sanitize/angular-sanitize.min.js',
+                    'bower_components/angular-touch/angular-touch.min.js',
                     'src/app/app.js',
                     'src/app/controllers/*.js'
                 ],
--- src/app/controllers/main.js
+++ src/app/controllers/main.js
@@ -8,19 +8,31 @@
  * Controller of the app
  */
 angular.module('app')
-    .controller('MainCtrl', function ($scope) {
+    .controller('MainCtrl', function ($scope, $uibModal) {
         $scope.date = new Date();
-
-        $scope.notificationSubmit = function() {
-
+        $scope.contactUsModal = function () {
+            $uibModal.open({
+                templateUrl: 'views/main/modals/contactUsModal.html',
+                controller: 'ModalInstanceCtrl'
+            });
         };
-
-        $scope.signUpSubmit = function() {
-
+        $scope.signUpModal = function () {
+            $uibModal.open({
+                templateUrl: 'views/main/modals/signUpModal.html',
+                controller: 'ModalInstanceCtrl'
+            });
         };
-
-        $scope.contactUsSubmit = function() {
-
+        $scope.notifiedSubmit = function () {
+            console.log($scope.notifiedForm);
         };
-
+    }).controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
+        $scope.close = function () {
+            $uibModalInstance.close();
+        };
+        $scope.contactUsSubmit = function () {
+            console.log($scope.contactForm);
+        };
+        $scope.signUpSubmit = function () {
+            console.log($scope.signUpForm);
+        };
     });
